7944db906177b1b649e6d1f88b6f7bdea46140ec
Caner Ilbin <caner.ilbin@student.pxl.be>
2022-11-14 17:09:45 +0100

    - speciale kaarten (koning, vrouw, boer toegevoegd) - deze een standard value van 10 gegeven

--- BlackjackApplicatie/BlackjackApplicatie/MainWindow.xaml.cs
+++ BlackjackApplicatie/BlackjackApplicatie/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,29 +25,101 @@ namespace BlackjackApplicatie
         {
             InitializeComponent();
         }
-       
+
 
         private void BtnDeel_Click(object sender, RoutedEventArgs e)
         {
-            // hier maak ik een array van de kaarten + de kaartvalues
-            string[] kaarten = { "Klaver", "Schoppen", "Ruiten", "Harten", "Klaveren koning", "Klaveren vrouw",
-            "Klaveren boer", "Schoppen koning", "Schoppen vrouw", "Schoppen boer", "Ruiten koning",
-            "Ruiten vrouw", "Ruiten boer", "Harten koning", "Harten vrouw", "Harten boer" };
+            // hier maak ik een array van de kaarten + de kaartvalues + de speciale kaarten
+            string[] kaarten = { "Klaveren", "Klaveren Koning", "Klaveren Vrouw", "Klaveren Boer", 
+                "Schoppen", "Schoppen Koning", "Schoppen Vrouw", "Schoppen Boer", "Ruiten", "Ruiten Koning",
+                "Ruiten Boer", "Ruiten Vrouw", "Harten", "Harten Koning", "Harten Vrouw", "Harten Boer" };
+            //  ArrayList kaartvalues = new ArrayList();
+            // kaartvalues.Add(1);
+            // kaartvalues.Add(2);
+            //  kaartvalues.Add("Koning");
             int[] kaartvalues = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
             // hier generate ik een random voor de kaart, en de kaartvalue
             Random rndkaart = new Random();
             Random rndvalue = new Random();
-            string kaart = kaarten[rndkaart.Next(kaarten.Length)];
-            int kaartvalue = kaartvalues[rndvalue.Next(kaartvalues.Length)];
-            // kaart toevoegen aan de textbox
-            StringBuilder kaartspeler = new StringBuilder();
-            kaartspeler.AppendLine($"{kaart} {kaartvalue}");
-            txtSpeler.Text = kaartspeler.ToString();
-           
+            StringBuilder kaartenSpeler = new StringBuilder();
+            StringBuilder kaartenBank = new StringBuilder();
+            // de kaarten worden toegewezen aan de speler
+            string kaart1Speler = kaarten[rndkaart.Next(kaarten.Length)];
+            int kaartvalue1Speler = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            //  int kaartvalue1Speler = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            string kaart2Speler = kaarten[rndkaart.Next(kaarten.Length)];
+            int kaartvalue2Speler = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            //grote if bool voor speler --> als kaart koning, boer, of vrouw is (value 10, anders normal value)
+            if (kaart1Speler.Contains("Koning") == true || kaart1Speler.Contains("Vrouw") == true || kaart1Speler.Contains("Boer") == true)
+            {
+                kaartvalue1Speler = 10;
+                kaartenSpeler.AppendLine($"{kaart1Speler}");
+                
+            }
+            else
+            {
+                kaartenSpeler.AppendLine($"{kaart1Speler} {kaartvalue1Speler}");
+            }
+            if (kaart2Speler.Contains("Koning") == true || kaart2Speler.Contains("Vrouw") == true || kaart2Speler.Contains("Boer") == true)
+            {
+                kaartvalue2Speler = 10;
+                kaartenSpeler.AppendLine($"{kaart2Speler}");  
+            }
+            else
+            {
+                kaartenSpeler.AppendLine($"{kaart2Speler} {kaartvalue2Speler}");
+            }
+            txtSpeler.Text = kaartenSpeler.ToString();
+            /// bank
+            string kaart1Bank = kaarten[rndkaart.Next(kaarten.Length)];
+            int kaartvalue1Bank = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            string kaart2Bank = kaarten[rndkaart.Next(kaarten.Length)];
+            int kaartvalue2Bank = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            if (kaart1Bank.Contains("Koning") == true || kaart1Bank.Contains("Vrouw") == true || kaart1Bank.Contains("Boer") == true)
+            {
+                kaartvalue1Bank = 10;
+                kaartenBank.AppendLine($"{kaart1Bank}");
+            }
+            else
+            {
+                kaartenBank.AppendLine($"{kaart1Bank} {kaartvalue1Bank}");
+            }
+            if (kaart2Bank.Contains("Koning") == true || kaart2Bank.Contains("Vrouw") == true || kaart2Bank.Contains("Boer") == true)
+            {
+                kaartvalue2Bank = 10;
+                kaartenBank.AppendLine($"{kaart2Bank}");
+            }
+            else
+            {
+                kaartenBank.AppendLine($"{kaart2Bank} {kaartvalue2Bank}");
+            }
+            txtBank.Text = kaartenBank.ToString();
+            int somSpeler = kaartvalue1Speler + kaartvalue2Speler;
+            int somBank = kaartvalue1Bank + kaartvalue2Bank;
+            LblscoreSpeler.Content = somSpeler.ToString();
+            LblscoreBank.Content = somBank.ToString();
+
+            //// de kaarten worden toegewezen aan de bank
+            //string kaart1Bank = kaarten[rndkaart.Next(kaarten.Length)];
+            //int kaartvalue1Bank = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            //string kaart2Bank = kaarten[rndkaart.Next(kaarten.Length)];
+            //int kaartvalue2Bank = kaartvalues[rndvalue.Next(kaartvalues.Length)];
+            //// kaarten speler toevoegen aan de textbox
+            //StringBuilder kaartenSpeler = new StringBuilder();
+            //kaartenSpeler.AppendLine($"{kaart1Speler} {kaartvalue1Speler}");
+            //kaartenSpeler.AppendLine($"{kaart2Speler} {kaartvalue2Speler}");
+            //txtSpeler.Text = kaartenSpeler.ToString();
+            //// kaarten bank toevoegen aan de textbox
+            //StringBuilder kaartenBank = new StringBuilder();
+            //kaartenBank.AppendLine($"{kaart1Bank} {kaartvalue1Bank}");
+            //kaartenBank.AppendLine($"{kaart2Bank} {kaartvalue2Bank}");
+            //txtBank.Text = kaartenBank.ToString();
+            //// som van de kaarten van speler en bank word gemaakt
+            //int somSpeler = kaartvalue1Speler + kaartvalue2Speler;
+            //int somBank = kaartvalue1Bank + kaartvalue2Bank;
+            //LblscoreSpeler.Content = somSpeler.ToString();
+            //LblscoreBank.Content = somBank.ToString();
+
         }
-
-
-
-
     }
 }
